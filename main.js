f097768c86d6e3bba1066de0070758f6eaf6e206
oarkon <oarkon@hotmail.com>
2019-08-31 19:11:48 +0300

    Basic Cipher

---
+++ main.js
@@ -0,0 +1,37 @@
+const slider = document.forms[0].shift;
+const range = document.getElementById("range");
+range.innerHTML = slider.value;
+slider.oninput = function () {
+    range.innerHTML = this.value;
+}
+
+
+const Crpytohabet = [
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z', "!", "@", "#", "$",
+    "%", "^", "&", "*", "(", ")",
+    "0", "1", "2", "3", "4", "5",
+    "6", "7", "8", "9"
+]
+
+const form = document.forms[0];
+const output = document.getElementById('output');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    output.innerText = [...form.plaintext.value].map(char => encrypt(char)).join('')
+})
+
+encrypt = (char) => {
+    const shift = Number(form.shift.value);
+    if (Crpytohabet.includes(char.toUpperCase())) {
+        const position = Crpytohabet.indexOf(char.toUpperCase())
+        const newPosition = (position + shift) % 45;
+        return Crpytohabet[newPosition];
+    }
+    else
+        return char
+}
